msft-mirror-aosp/platform.test.dittosuite

commit 76bcc2f8e1cd867df68f621a4f76b9345d05d7c0

Update schema
Update schema, so that it includes Global and Main directives, and has "repeat" fields as optional. In addtion, add absolute_path field to the Global directive and update example files to match the schema.

Bug: 194378950
Test: dittobench_test
Signed-off-by: Robertas Norkus <robertasn@google.com>
Change-Id: I0b7e5c8f1ee28e050bc50abc755d657bce77eedf

src/parser.cpp
@@ -38,7 +38,7 @@ std::unique_ptr<Instruction> Parser::Parse() {
   google::protobuf::io::FileInputStream file_input(open(file_path_.c_str(), O_CLOEXEC));
   google::protobuf::TextFormat::Parse(&file_input, benchmark.get());
 
-  return InstructionFactory::CreateFromProtoInstruction(benchmark->benchmark());
+  return InstructionFactory::CreateFromProtoInstruction(benchmark->main());
 }
 
 void Parser::SetFilePath(const std::string& file_path) {

example/create_delete_file.ditto
@@ -1,20 +1,19 @@
-benchmark: {
+main: {
   instruction_set: {
     instructions: [
       {
         instruction_create_file: {
-          file: "/data/local/tmp/newfile2.txt"
-        },
-        repeat: 1
+          file: "newfile2.txt"
+        }
       },
       {
         instruction_delete_file: {
-          file: "/data/local/tmp/newfile2.txt"
-        },
-        repeat: 1
+          file: "newfile2.txt"
+        }
       }
     ]
-  },
-  repeat: 1
+  }
 },
-repeat: 1
+global {
+  absolute_path: "/data/local/tmp/";
+}

example/create_file.ditto
@@ -1,7 +1,9 @@
-benchmark: {
+main: {
   instruction_create_file: {
-    file: "/data/local/tmp/newfile2.txt"
-  },
-  repeat: 1
+    file: "newfile2.txt"
+  }
 },
-repeat: 1
+global {
+  absolute_path: "/data/local/tmp/";
+}
+

example/delete_file.ditto
@@ -1,7 +1,8 @@
-benchmark: {
+main: {
   instruction_delete_file: {
-    file: "/data/local/tmp/newfile2.txt"
-  },
-  repeat: 1
+    file: "newfile2.txt"
+  }
 },
-repeat: 1
+global {
+  absolute_path: "/data/local/tmp/";
+}

include/ditto/instruction.h
@@ -14,6 +14,8 @@
 
 #pragma once
 
+#include <string>
+
 namespace dittosuite {
 
 class Instruction {
@@ -25,8 +27,13 @@ class Instruction {
   void Run();
   virtual void TearDown() = 0;
 
+  static void SetAbsolutePath(const std::string& absolute_path);
+  static std::string GetAbsolutePath();
+
  private:
   virtual void RunSingle() = 0;
+
+  static std::string absolute_path_;
   int repeat_;
 };
 

schema/benchmark.proto
@@ -11,7 +11,7 @@ message InstructionDeleteFile {
 }
 
 message Instruction {
-  required int32 repeat = 1;
+  optional int32 repeat = 1;
   oneof instruction_oneof {
     InstructionSet instruction_set = 2;
     InstructionCreateFile instruction_create_file = 3;
@@ -23,7 +23,11 @@ message InstructionSet {
   repeated Instruction instructions = 1;
 }
 
+message Global {
+  optional string absolute_path = 1 [default = ""];
+}
+
 message Benchmark {
-  required Instruction benchmark = 1;
-  required int32 repeat = 2;
+  required Instruction main = 1;
+  required Global global = 2;
 }

src/create_file.cpp
@@ -21,7 +21,8 @@
 
 namespace dittosuite {
 
-CreateFile::CreateFile(int repeat, const std::string& file) : Instruction(repeat), file_(file) {}
+CreateFile::CreateFile(int repeat, const std::string& file)
+    : Instruction(repeat), file_(Instruction::GetAbsolutePath() + file) {}
 
 void CreateFile::SetUp() {}
 

src/delete_file.cpp
@@ -20,7 +20,8 @@
 
 namespace dittosuite {
 
-DeleteFile::DeleteFile(int repeat, const std::string& file) : Instruction(repeat), file_(file) {}
+DeleteFile::DeleteFile(int repeat, const std::string& file)
+    : Instruction(repeat), file_(Instruction::GetAbsolutePath() + file) {}
 
 void DeleteFile::SetUp() {}
 

commit 2c5bf8a28269ec62e3363b0d340a894c66a5bb2e

Fix setting absolute_path in parser
Fixes: 76bcc2f8e1 ("Update schema"). Missed during rebase. Without this,
absolute_path would stay as empty string even though absolute_path is
defined in the .ditto file.

Bug: 194378950
Test: dittobench_test
Signed-off-by: Robertas Norkus <robertasn@google.com>
Change-Id: I85305b33a8ed7037e5fdee66f2754bba4ab4a166

src/parser.cpp
@@ -38,6 +38,8 @@ std::unique_ptr<Instruction> Parser::Parse() {
   google::protobuf::io::FileInputStream file_input(open(file_path_.c_str(), O_CLOEXEC));
   google::protobuf::TextFormat::Parse(&file_input, benchmark.get());
 
+  Instruction::SetAbsolutePath(benchmark->global().absolute_path());
+
   return InstructionFactory::CreateFromProtoInstruction(benchmark->main());
 }
 