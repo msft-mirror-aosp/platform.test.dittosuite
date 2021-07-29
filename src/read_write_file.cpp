6b43cc54504010181dd75a4404b545fc1d589fcb
Robertas Norkus <robertasn@google.com>
2021-07-29 14:07:06 +0000

    Create shared class ReadWriteFile
    ReadFile and WriteFile has a lot in common (same
    SetUp and member variables), so shared class ReadWriteFile
    will help keeping the code DRY.
    
    Bug: 194366051
    Test: manual
    Signed-off-by: Robertas Norkus <robertasn@google.com>
    Change-Id: I3f3841f299f53976d28b9d1db5bbf80187407fda

--- src/read_write_file.cpp
+++ src/read_write_file.cpp
@@ -12,8 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <ditto/write_file.h>
+#include <ditto/read_write_file.h>
 
+#include <fcntl.h>
 #include <unistd.h>
 
 #include <cstdint>
@@ -25,20 +26,19 @@
 
 namespace dittosuite {
 
-WriteFile::WriteFile(int repeat, int64_t size, int64_t block_size, ReadWriteType type,
-                     u_int32_t seed, bool fsync, int input_fd_key)
+ReadWriteFile::ReadWriteFile(int repeat, int64_t size, int64_t block_size, ReadWriteType type,
+                             u_int32_t seed, int input_fd_key)
     : Instruction(repeat),
       size_(size),
       block_size_(block_size),
       type_(type),
       gen_(seed),
-      fsync_(fsync),
       input_fd_key_(input_fd_key) {
   buffer_ = std::make_unique<char[]>(block_size_);
   std::fill(buffer_.get(), buffer_.get() + block_size_, 170);  // 170 = 10101010
 }
 
-void WriteFile::SetUp() {
+void ReadWriteFile::SetUp() {
   int fd = std::get<int>(SharedVariables::Get(input_fd_key_));
   int64_t file_size = GetFileSize(fd);
 
@@ -71,6 +71,12 @@ void WriteFile::SetUp() {
 
   Instruction::SetUp();
 }
+
+void ReadWriteFile::RunSingle() {}
+
+WriteFile::WriteFile(int repeat, int64_t size, int64_t block_size, ReadWriteType type,
+                     u_int32_t seed, bool fsync, int input_fd_key)
+    : ReadWriteFile(repeat, size, block_size, type, seed, input_fd_key), fsync_(fsync) {}
 
 void WriteFile::RunSingle() {
   int fd = std::get<int>(SharedVariables::Get(input_fd_key_));
