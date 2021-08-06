54894cb33688947af9e7d80c243bfcfc54d19ff1
Lucia Lup <lucialup@google.com>
2021-08-06 12:51:52 +0000

    Added statistics table output
    Method PrintStatisticsTable prints a table containing
    information regarding the statistics (every instruction
    path/name, with the corresponding min, max, mean).
    
    The first column of the table represents the instruction
    path. It displays up to 2 'parent' instructions in the
    instruction tree. For example,
    instructionA/instructionB/instructionC/instructionD
    will be printed as
    ..instructionC/instructionD
    and the same for longer instruction paths.
    
    Bug: 195113426
    Test: manual
    Signed-off-by: Lucia Lup <lucialup@google.com>
    Change-Id: I5f6391d44e24ed37854e2899930c54b0aa8b4a76

--- src/result.cpp
+++ src/result.cpp
@@ -15,7 +15,13 @@
 #include <ditto/result.h>
 #include <ditto/statistics.h>
 
+#include <algorithm>
+#include <iomanip>
 #include <iostream>
+#include <string>
+
+const int kTableWidth = 137;  // table width; can be adjusted in case of longer instruction paths
+const char* kTableDivider = " | ";  // table character divider
 
 namespace dittosuite {
 
@@ -25,6 +31,7 @@ Result::Result(const std::string& name, std::vector<timespec> time_samples)
 std::string Result::GetName() {
   return name_;
 }
+
 std::vector<timespec> Result::GetTimeSamples() {
   return time_samples_;
 }
@@ -64,5 +71,66 @@ void Result::Print(const std::string& instruction_path) {
   for (const auto& sub_result : sub_results_) {
     sub_result->Print(next_instruction_path);
   }
+}
+
+void PrintTableBorder() {
+  std::cout << std::endl;
+  for (int i = 0; i < kTableWidth; i++) std::cout << "-";
+  std::cout << std::endl;
+}
+
+void PrintStatisticsTableHeader() {
+  std::cout << "\x1b[1m";  // beginning of bold
+  PrintTableBorder();
+  std::cout << "| ";  // beginning of table row
+  std::cout << std::setw(70) << std::left << "Instruction name";
+  std::cout << kTableDivider;
+  std::cout << std::setw(18) << std::left << " Min";
+  std::cout << kTableDivider;
+  std::cout << std::setw(18) << std::left << " Max";
+  std::cout << kTableDivider;
+  std::cout << std::setw(18) << std::left << " Mean";
+  std::cout << kTableDivider;
+  PrintTableBorder();
+  std::cout << "\x1b[0m";  // ending of bold
+}
+
+void PrintTimespecInTable(const timespec& t) {
+  std::cout << std::setw(3) << t.tv_sec << "s, ";
+  std::cout << std::setw(10) << t.tv_nsec << "ns";
+}
+
+// Recursive function to print one row at a time
+// of statistics table content (the instruction path, min, max and mean).
+void Result::PrintStatisticsTableContent(const std::string& instruction_path) {
+  std::cout << "| ";  // started new row
+  std::string next_instruction_path =
+      instruction_path + (instruction_path != "" ? "/" : "") + name_;
+  int subinstruction_level =
+      std::count(next_instruction_path.begin(), next_instruction_path.end(), '/');
+  // If the instruction path name contains too many subinstrions,
+  // print only the last 2 preceded by "../".
+  if (subinstruction_level > 2) {
+    std::size_t first_truncate_pos = next_instruction_path.find("/");
+    next_instruction_path = ".." + next_instruction_path.substr(first_truncate_pos);
+  }
+
+  std::cout << std::setw(70) << std::left << next_instruction_path << std::right;
+  std::cout << kTableDivider;
+  PrintTimespecInTable(min_);
+  std::cout << kTableDivider;
+  PrintTimespecInTable(max_);
+  std::cout << kTableDivider;
+  PrintTimespecInTable(mean_);
+  std::cout << kTableDivider;  // ended current row
+  PrintTableBorder();
+  for (const auto& sub_result : sub_results_) {
+    sub_result->PrintStatisticsTableContent(next_instruction_path);
+  }
+}
+
+void Result::PrintStatisticsTable() {
+  PrintStatisticsTableHeader();
+  PrintStatisticsTableContent("");
 }
 }  // namespace dittosuite
