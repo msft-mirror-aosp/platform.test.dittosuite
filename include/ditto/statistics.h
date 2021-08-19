f2f5a5aa2c4018396866c45fdbf8e2fd4ad99e49
Lucia Lup <lucialup@google.com>
2021-08-19 08:53:20 +0000

    Implement statistics templates
    Implement statistics templates.
    The statistics functions are converted into
    templates functions so they can be perfomed
    on different data types.
    
    Bug: 195113227
    Test: manual
    Signed-off-by: Lucia Lup <lucialup@google.com>
    Change-Id: Ia1bb72eca2cbf770c48399ec2784d0bcc01d3ac0

--- include/ditto/statistics.h
+++ include/ditto/statistics.h
@@ -14,18 +14,75 @@
 
 #pragma once
 
-#include <time.h>
+#include <ditto/logger.h>
+#include <ditto/time_sampler.h>
 
+#include <cmath>
+#include <ctime>
+
+#include <algorithm>
 #include <memory>
+#include <numeric>
 #include <string>
 #include <vector>
 
 namespace dittosuite {
 
-timespec StatisticsGetMin(const std::vector<timespec>& time_samples);
-timespec StatisticsGetMax(const std::vector<timespec>& time_samples);
-timespec StatisticsGetMean(const std::vector<timespec>& time_samples);
-timespec StatisticsGetMedian(const std::vector<timespec>& time_samples);
-double StatisticsGetSd(const std::vector<timespec>& time_samples);
+template <class T>
+T StatisticsGetMin(const std::vector<T>& samples) {
+  return *std::min_element(samples.begin(), samples.end());
+}
+
+template <class T>
+T StatisticsGetMax(const std::vector<T>& samples) {
+  return *std::max_element(samples.begin(), samples.end());
+}
+
+template <class T>
+T StatisticsGetMean(const std::vector<T>& samples) {
+  if (samples.empty()) PLOGF("Cannot compute statistics on an empty vector");
+
+  T result = std::accumulate(samples.begin(), samples.end(), T{});
+  return result / samples.size();
+}
+
+template <class T>
+T StatisticsGetMedian(const std::vector<T>& samples) {
+  auto my_vector_copy = samples;
+  auto n = samples.size();
+
+  if (n % 2) {
+    // odd number of elements, the median is the element in the middle
+    std::nth_element(my_vector_copy.begin(), my_vector_copy.begin() + n / 2, my_vector_copy.end());
+    return my_vector_copy[n / 2];
+  } else {
+    // even number of elements, the median is the average between the two middle elements
+    std::nth_element(my_vector_copy.begin(), my_vector_copy.begin() + n / 2, my_vector_copy.end());
+    std::nth_element(my_vector_copy.begin(), my_vector_copy.begin() + (n - 1) / 2,
+                     my_vector_copy.end());
+    T result = my_vector_copy[n / 2] + my_vector_copy[(n - 1) / 2];
+    return result / 2;
+  }
+}
+
+
+// The standard deviation sd of a population of N samples, where x_i is the
+// i-th sample and x is the average among all the samples is computed as:
+//
+// sd = sqrt( sum( (x_i - x)^2 ) / N )
+double StatisticsGetSd(const std::vector<timespec>& samples) {
+  double mean = TimespecToNs(StatisticsGetMean(samples));
+  double variance;
+
+  variance = 0.0;
+  for (const auto& s : samples) {
+    double deviation = TimespecToNs(s) - mean;
+    double deviation_square = std::pow(deviation, 2);
+    variance += deviation_square;  // TODO(lucialup): add overflow error handling
+  }
+  variance /= samples.size();
+
+  return std::sqrt(variance);
+}
 
 }  // namespace dittosuite
